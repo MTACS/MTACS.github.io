cf8708802d0d1c59eb2b5b4960cd233d85ee78a4
MTAC <mtactheming@gmail.com>
2017-06-21 07:57:47 -0400

    no message

--- packages.sh
+++ packages.sh
@@ -3,3 +3,4 @@ dpkg-deb -bZgzip projects/iOS11System debs
 dpkg-deb -bZgzip projects/Mantra debs
 dpkg-deb -bZgzip projects/Freya debs
 dpkg-deb -bZgzip projects/Dock debs
+dpkg-deb -bZgzip projects/iOS\ 3\ Dock debs
