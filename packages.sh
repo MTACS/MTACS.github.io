cef2acc8c7830b5a6fa46a7bf570e9f363805b50
MTAC <mtactheming@gmail.com>
2017-06-24 00:33:49 -0400

    no message

--- packages.sh
+++ packages.sh
@@ -4,3 +4,4 @@ dpkg-deb -bZgzip projects/Mantra debs
 dpkg-deb -bZgzip projects/Freya debs
 dpkg-deb -bZgzip projects/Dock debs
 dpkg-deb -bZgzip projects/iOS\ 3\ Dock debs
+dpkg-deb -bZgzip projects/RemotePower debs
