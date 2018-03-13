9bc34a2e92b5d6a3820541121fee615a92d9936f
MTAC <mtactheming@gmail.com>
2018-03-13 14:30:34 -0400

    Added PurchaseEnabler103

--- packages.sh
+++ packages.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/Mantra debs
 dpkg-deb -bZgzip projects/Freya debs
-dpkg-deb -bZgzip projects/Dock debs
+dpkg-deb -bZgzip projects/PurchaseEnabler103 debs
 dpkg-deb -bZgzip projects/iOS\ 3\ Dock debs
-dpkg-deb -bZgzip projects/Remote\ Helper debs
 dpkg-deb -bZgzip projects/Terminal\ Power debs
-dpkg-deb -bZgzip projects/Verbose\ Springy debs
 dpkg-deb -bZgzip projects/MTAC\ LockHTML debs
 dpkg-deb -bZgzip projects/No\ LS\ Battery debs
 dpkg-deb -bZgzip projects/Runtime\ Browser debs
 dpkg-deb -bZgzip projects/Rounded\ Status debs
-dpkg-deb -bZgzip projects/phoenixfix debs
 dpkg-deb -bZgzip projects/SleekSheets debs
 dpkg-deb -bZgzip projects/substratealert debs
 dpkg-deb -bZgzip projects/SimpleFolders debs
-dpkg-deb -bZgzip projects/Jailbreak\ Springy debs
 dpkg-deb -bZgzip projects/Apple\ Debug\ Server debs
 dpkg-deb -bZgzip projects/TigrisPapers debs
 dpkg-deb -bZgzip projects/NoIGAccountLimit debs
-dpkg-deb -bZgzip projects/No\ Page\ Dots debs
-dpkg-deb -bZgzip projects/Stasher debs
 dpkg-deb -bZgzip projects/iOS\ 11 debs
 dpkg-deb -bZgzip projects/CyPatreon debs
