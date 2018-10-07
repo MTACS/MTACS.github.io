273f816163ce125e9ae0f8258236388f008c17af
MTAC <mtactheming@gmail.com>
2018-10-07 18:58:48 -0400

    Added NoTabBarLabelXI

--- packages.sh
+++ packages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/Mantra debs
-dpkg-deb -bZgzip projects/Freya debs
-dpkg-deb -bZgzip projects/PurchaseEnabler103 debs
-dpkg-deb -bZgzip projects/iOS\ 3\ Dock debs
+# dpkg-deb -bZgzip projects/Mantra debs
+# dpkg-deb -bZgzip projects/Freya debs
+# dpkg-deb -bZgzip projects/PurchaseEnabler103 debs
+# dpkg-deb -bZgzip projects/iOS\ 3\ Dock debs
 dpkg-deb -bZgzip projects/Terminal\ Power debs
-dpkg-deb -bZgzip projects/MTAC\ LockHTML debs
-dpkg-deb -bZgzip projects/No\ LS\ Battery debs
+# dpkg-deb -bZgzip projects/MTAC\ LockHTML debs
+# dpkg-deb -bZgzip projects/No\ LS\ Battery debs
 dpkg-deb -bZgzip projects/Runtime\ Browser debs
 dpkg-deb -bZgzip projects/Rounded\ Status debs
 dpkg-deb -bZgzip projects/SleekSheets debs
@@ -14,10 +14,11 @@ dpkg-deb -bZgzip projects/SimpleFolders debs
 dpkg-deb -bZgzip projects/Apple\ Debug\ Server debs
 dpkg-deb -bZgzip projects/TigrisPapers debs
 dpkg-deb -bZgzip projects/NoIGAccountLimit debs
-dpkg-deb -bZgzip projects/iOS\ 11 debs
-dpkg-deb -bZgzip projects/CyPatreon debs
+# dpkg-deb -bZgzip projects/iOS\ 11 debs
+# dpkg-deb -bZgzip projects/CyPatreon debs
 dpkg-deb -bZgzip projects/BarVol debs
-dpkg-deb -bZgzip projects/SmoothNotifications debs
+# dpkg-deb -bZgzip projects/SmoothNotifications debs
 dpkg-deb -bZgzip projects/SimpleSettings debs
-dpkg-deb -bZgzip projects/NOIGTV debs
+# dpkg-deb -bZgzip projects/NOIGTV debs
 dpkg-deb -bZgzip projects/CleanMusicCC debs
+dpkg-deb -bZgzip projects/NoTabBarLabelsXI debs
